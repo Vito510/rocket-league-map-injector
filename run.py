c9af83a7a1ba8281fb51264eef2458c202005d2b
Vito <73427833+Vito510@users.noreply.github.com>
2020-10-30 21:57:13 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
+import glob
 import shutil
 path = '.'
 dstFile = open("dst.txt", "r")
 gameDir = dstFile.read()+'/TAGame/CookedPCConsole'
 print("Created by https://github.com/Vito510\n")
 i = int(1)
+fc = int(0)
 directory_contents = os.listdir("MapFolders")
 for item in directory_contents:
     print(i,"> ",item)
@@ -23,18 +25,29 @@ for item in directory_contents:
 path = "MapFolders/"+path
 directory_contents = os.listdir(path)
 item = ''
-for item in directory_contents:
-    if os.path.isdir(item):
-        item = item
 
-path = path+"/"+item
-gameDir = gameDir+"/Labs_Utopia_P.upk"
-print("\nsource > ",path)
-print("destination > ",gameDir)
-shutil.copyfile(path,gameDir)
-
-print("\nDone!")
-input("press ENTER to close")
+array = os.listdir(path)
+for x in range(len(array)):
+    if array[x].split(".")[-1] == 'upk' or array[x].split(".")[-1] == 'udk':
+        item = array[x]
+        fc = fc + 1
+        
+if fc == 1:
+    path = path+"/"+item
+    gameDir = gameDir+"/Labs_Utopia_P.upk"
+    print("\nsource > ",path)
+    print("destination > ",gameDir)
+    shutil.copyfile(path,gameDir)
+    if len(array) > 1:
+        print("\nWarning: \nfolder contains more than just a upk/udk file.\nSome maps require more than just a upk/udk file to work! \nThis program currently only moves the udk/upk file to the 'CookedPCConsole' folder")
+    print("\nDone!")
+    input("press ENTER to close")
+elif fc > 1:
+    print("\nError: Multiple upk/udk files found!")
+    input("press ENTER to close")
+else:
+    print("\nError: No upk/udk files found!")
+    input("press ENTER to close")
 
 
    
